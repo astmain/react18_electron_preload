63ef87bd0df55e28137000706389c2268cae040f
xupeng <1311192345@qq.com>
2025-04-25 18:36:15 +0800

    备份

--- middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    // 允许所有域名进行跨域调用
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // 允许的请求头
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // 允许的请求方法
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-};
