9e4fc5e9cab537d5338a7c8637dd949a9654034c
xupeng <1311192345@qq.com>
2025-04-28 09:45:38 +0800

    11111

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import {createRoot} from 'react-dom/client'
 import {Provider} from 'react-redux'
 import store from './store/index'
 import './css.css'
+import 'normalize.css'
 import App from './App.jsx'
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
